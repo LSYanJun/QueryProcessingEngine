30e130482d620581a3c031a5463b9fbe76445623
Gong Cheng <chenggongtc@gmail.com>
2015-05-04 09:52:10 -0400

    complete table finished

--- engine.cpp
+++ engine.cpp
@@ -26,7 +26,7 @@ vector<string> S,V,F,G,W; // seperate the whole line information into vector
 int N; // the number of grouping variables
 vector< vector<string> > VECO; // the two dimension vector for the range of grouping variables (VECO[0][0] means the 
 				// first condition of the first grouping variable)
-vector< vector<string> > aggrPrior; // store the scan order for the grouping variables
+vector< vector<int> > aggrPrior; // store the scan order for the grouping variables
 vector<string> hashPara; // store the parameters for the hash function in the program
 //--------------------------------------
 
@@ -38,6 +38,8 @@ string convertToString(int temp); // convert int to string (to_string)
 string attrType(string attr); // get the type of the grouping attributes
 void setPrior(); // optimize the scan order for the grouping variables
 void setHashPara(); // set the parameters for the hash function in the program
+string convertExp(string exp, string argu); // convert the expression to C program
+string convertAggregates(string agg, string argu); // convert the aggregates
 //--------------------------------------
 
 //--------------------------------------
@@ -227,6 +229,10 @@ void operandProcess()
 		ss >> temp;
 		if(temp=="") continue;
 		if(temp[temp.size()-1]==',') temp.erase(temp.size()-1,1);
+		if(temp == "and")
+			temp = "&&";
+		else if (temp == "or")
+			temp = "||";
 		W.push_back(temp);
 	}
 }
@@ -277,9 +283,9 @@ void writeFirstPart()
 	"struct {" << endl <<
 	"	char	*cust;" << endl <<
 	"	char	*prod;" << endl <<
-	"	short    dd;" << endl <<
-	"	short    mm;" << endl <<
-	"	short	 yy;" << endl <<
+	"	short    day;" << endl <<
+	"	short    month;" << endl <<
+	"	short	 year;" << endl <<
 	"	char	*state;" << endl <<
 	"	long	 quant;" << endl <<
 	"}	sale_rec;" << endl <<
@@ -295,7 +301,20 @@ void writeSecondPart()
 	"// FUNCTION PROTOTYPE declaration" << endl <<
 	"//----------------------------------------------------------------------" << endl << endl <<
 	"void	initialMFS();" << endl <<
-	"unsigned int	hashFunc(char*);" << endl << endl <<
+	"unsigned int	hashFunc(";
+	if(hashPara.size() != 0)
+	{
+		for(int i = 0; i < hashPara.size() - 1; i++)
+		{
+			string type = attrType(hashPara[i]);
+			if(type.find("char") != -1) cout << "char*, ";
+			else cout << type << ", ";
+		}
+		string type = attrType(hashPara[hashPara.size() - 1]);
+		if(type.find("char") != -1) cout << "char*";
+		else cout << type;
+	}
+	cout << ");" << endl <<
 	"//----------------------------------------------------------------------" << endl <<
 	"int main(int argc, char* argv[])" << endl <<
 	"//----------------------------------------------------------------------" << endl <<
@@ -356,53 +375,184 @@ void writeCompleteTable() //assigned: Gong Cheng
 {
 	cout << 
 	"   //optimization relative aggrigate functions  assigned: Gong Cheng" << endl;
-	cout <<
-	"   for(i = 0; i < " << aggrPrior.size() - 1 << "; i++)" << endl <<
-	"   {" << endl <<
-	"	   // Open cursor" << endl <<
-	"	   EXEC SQL OPEN mycursor;" << endl <<
-	"	   // Fetch Data" << endl <<
-	"	   EXEC SQL FETCH FROM mycursor INTO :sale_rec; //fetch the first row" << endl <<
-	"	   while (sqlca.sqlcode == 0) {" << endl <<
-	"		unsigned int hash = hashFunc(";
-	if(hashPara.size() != 0)
+
+	for(int p = 1; p < aggrPrior.size(); p++)
 	{
-		for(int i = 0; i < hashPara.size() - 1; i++)
-		cout << "sale_rec." << hashPara[i] << ", ";
-		cout << "sale_rec." << hashPara[hashPara.size() - 1];
+		cout << "   //Scan " << p << endl <<
+		"   // Open cursor" << endl <<
+		"   EXEC SQL OPEN mycursor;" << endl <<
+		"   // Fetch Data" << endl <<
+		"   EXEC SQL FETCH FROM mycursor INTO :sale_rec; //fetch the first row" << endl <<
+		"   while (sqlca.sqlcode == 0) {" << endl;
+		if(W.size() != 0)  // where clause condition
+		{
+			cout <<"    if(";
+			for(int i = 0; i < W.size(); i++)
+			{
+				cout << convertExp(W[i], "sale_rec") << " ";
+			}
+			cout << ")" << endl <<
+			"    {" << endl;
+		}
+
+		cout <<
+		"	unsigned int hash = hashFunc(";
+		if(hashPara.size() != 0)
+		{
+			for(int i = 0; i < hashPara.size() - 1; i++)
+			cout << "sale_rec." << hashPara[i] << ", ";
+			cout << "sale_rec." << hashPara[hashPara.size() - 1];
+		}
+		cout << ");" << endl <<
+		"	unsigned int hashScan;" << endl;
+		vector<int> MFvar, EMFvar; // MF 1 to 1, EMF 1 to n
+		for(int q = 0; q < aggrPrior[p].size(); q++) // split the MF variables and EMF variables
+		{
+			bool MFflag = true; // flag for the MF/EMF variable
+			int varNum = aggrPrior[p][q]; 
+			for(int i = 0; i < V.size(); i++)
+			{
+				bool findV = false; // flag for the existance of the grouping attribute
+				for(int j = 0; j < VECO[varNum - 1].size(); j++)
+				{
+					if(VECO[varNum - 1][j].find("="+V[i]) != -1 && 
+					  (VECO[varNum - 1][j].find("="+V[i]) + ("="+V[i]).length()) == VECO[varNum - 1][j].length())
+					{
+						findV = true; break;
+					}
+				}
+				if(!findV) 
+				{
+					MFflag = false;
+					break;
+				}
+			}
+			if(MFflag) MFvar.push_back(varNum);
+			else EMFvar.push_back(varNum);
+		}
+		if(MFvar.size() != 0) // process for MF variable
+		{
+			cout <<
+			"	hashScan = hash;" << endl <<
+			"	while(";
+			for(int i = 0; i < V.size() - 1; i++) // find the exact row
+			{
+				if(i) cout << "	      ";
+				if(attrType(V[i]).find("char") != -1)
+				cout << "strcmp(MFS[hashScan]." << V[i] << ", sale_rec." << V[i] << ") != 0 || " << endl;
+				else cout << "MFS[hashScan]." << V[i] << " != sale_rec." << V[i] << " || " << endl;
+			}
+			if(V.size() > 1) cout << "	      ";
+			if(attrType(V[V.size() - 1]).find("char") != -1)
+			cout << "strcmp(MFS[hashScan]." << V[V.size() - 1] << ", sale_rec." << V[V.size() - 1] << ") != 0)" << endl;
+			else cout << "MFS[hashScan]." << V[V.size() - 1] << " != sale_rec." << V[V.size() - 1] << ")" << endl;
+			cout <<
+			"	{" << endl <<
+			"		hashScan=(hashScan + 1) % 500;" << endl <<
+			"	}" << endl;
+		}
+		for(int q = 0; q < MFvar.size(); q++) // process for MF variable
+		{
+			if(VECO[MFvar[q] - 1].size() == (V.size() * 2 - 1)) continue;
+			cout << "	if(";
+			for(int i = 0; i < VECO[MFvar[q] - 1].size(); i++)
+			{
+				bool GAflag = false; // flag for grouping attribute
+				if(VECO[MFvar[q] - 1][i].find("&&") == -1 && VECO[MFvar[q] - 1][i].find("||") == -1)
+				for(int j = 0; j < V.size(); j++)
+				{
+					if(VECO[MFvar[q] - 1][i] == V[j] + "=" + V[j])
+					{
+						GAflag = true; break;
+					}
+				}
+				if(GAflag)
+				{
+					i++;
+				}
+				else
+				{
+					cout << convertExp(VECO[MFvar[q] - 1][i], "MFS[hashScan]") << " ";
+				}
+			}
+			cout << ")" << endl << 
+			"	{" << endl;
+			for(int i = 0; i < F.size(); i++)
+			{
+				string stringVar = "_" + convertToString(MFvar[q]) + "_";
+				if(F[i].find(stringVar) != -1)
+				{
+					if(F[i].find("sum") != -1)
+					{
+						string attr = F[i].substr(F[i].find_last_of('_') + 1);
+						cout << "		" << convertAggregates(F[i], "MFS[hashScan]") << "+=" << attr << ";" << endl;
+					}
+					else if(F[i].find("count") != -1)
+					{
+						cout << "		" << convertAggregates(F[i], "MFS[hashScan]") << "++ ;" << endl;
+					}
+				}
+			}
+			cout << "	}" << endl;
+		}
+		if(EMFvar.size() != 0) // process for EMF variable
+		{
+			cout <<
+			"	hashScan = hash;" << endl <<
+			"	for(i = 0; i < 500; i++)" << endl <<
+			"	{" << endl <<
+			"		if(MFS[hashScan].flag == 0) break;" << endl;
+			for(int q = 0; q < EMFvar.size(); q++) // process for EMF variable
+			{
+				cout << "		if(";
+				for(int i = 0; i < VECO[EMFvar[q] - 1].size(); i++)
+				{
+					if(i && (VECO[EMFvar[q] - 1][i] != "&&" && VECO[EMFvar[q] - 1][i] != "||")) cout << "		   ";
+					cout << convertExp(VECO[EMFvar[q] - 1][i], "MFS[hashScan]") << " ";
+					if(i < VECO[EMFvar[q] - 1].size() - 1 && 
+					(VECO[EMFvar[q] - 1][i] == "&&" || VECO[EMFvar[q] - 1][i] == "||")) cout << endl;
+				}
+				cout << ")" << endl <<
+				"		{" << endl;
+				for(int i = 0; i < F.size(); i++)
+				{
+					string stringVar = "_" + convertToString(EMFvar[q]) + "_";
+					if(F[i].find(stringVar) != -1)
+					{
+						if(F[i].find("sum") != -1)
+						{
+							string attr = F[i].substr(F[i].find_last_of('_') + 1);
+							cout << "			" << 
+							convertAggregates(F[i], "MFS[hashScan]") << "+=" << attr << ";" << endl;
+						}
+						else if(F[i].find("count") != -1)
+						{
+							cout << "			" << 
+							convertAggregates(F[i], "MFS[hashScan]") << "++;" << endl;
+						}
+					}
+				}
+				cout << "		}" << endl;
+			}
+			cout <<
+			"		hashScan=(hashScan + 1) % 500;" << endl <<
+			"	}" << endl;
+		}
+
+
+		if(W.size() != 0) cout << "    }" << endl;
+		cout <<
+	 	"     EXEC SQL FETCH FROM mycursor INTO :sale_rec; //fetch the rest rows" << endl <<
+		"   }" << endl <<
+		"   // Close cursor" << endl <<
+		"   EXEC SQL CLOSE mycursor;" << endl <<
+		"   //----------------------------------------------------------------------" << endl <<
+		endl;
 	}
-	cout << ");" << endl <<
-	"		while(strcmp(MFS[hash].cust,sale_rec.cust)!=0)" << endl <<
-	"		{" << endl <<
-	"			hash=(hash+1)%500;" << endl <<
-	"		}" << endl <<
-	"		if(strcmp(sale_rec.state,\"NY\")==0)" << endl <<
-	"		{" << endl <<
-	"			MFS[hash].sum_1_quant+=sale_rec.quant;" << endl <<
-	"			MFS[hash].count_1_quant++;" << endl <<
-	"		}" << endl <<
-	"		else if(strcmp(sale_rec.state,\"CT\")==0)" << endl <<
-	"		{" << endl <<
-	"			MFS[hash].sum_2_quant+=sale_rec.quant;" << endl <<
-	"			MFS[hash].count_2_quant++;" << endl <<
-	"		}" << endl <<
-	"		else if(strcmp(sale_rec.state,\"NJ\")==0)" << endl <<
-	"		{" << endl <<
-	"			MFS[hash].sum_3_quant+=sale_rec.quant;" << endl <<
-	"			MFS[hash].count_3_quant++;" << endl <<
-	"		}" << endl <<
- 	"	     EXEC SQL FETCH FROM mycursor INTO :sale_rec; //fetch the rest rows" << endl <<
-	"	   }" << endl <<
-	"	   // Close cursor" << endl <<
-	"	   EXEC SQL CLOSE mycursor;" << endl <<
-	"	   //----------------------------------------------------------------------" << endl <<
-	"   }" << endl <<
-	endl;
 }
 
 void writeOutput() //assigned: Yanjun Wu
-{
-	string title = "";
+{	string title = "";
 	string subTitle = "";
 	string temp = "";
 	for (int i = 0; i < S.size(); i++)
@@ -494,23 +644,19 @@ string attrType(string attr)
 
 void setPrior()
 {
-	vector<string> init;
-	for(int i = 0; i < F.size(); i++)
-	{
-		if(F[i].find("_0_") != -1) init.push_back(F[i]);
-	}
+	vector<int> init;
+	init.push_back(0);
 	int count = 0;
 	while(count < N)
 	{
-		for(int i = 0; i < VECO.size(); i++)
+		for(int i = 0; i < VECO.size(); i++) // try to find all the variables that have the same counting priority
 		{
-			bool existFlag = false;
-			string variableNum = "_" + convertToString(i + 1) + "_";
-			for(int p = 0; p < aggrPrior.size(); p++)
+			bool existFlag = false; // the existing flag
+			for(int p = 0; p < aggrPrior.size(); p++) // if the variable's priority is set, continue
 			{
 				for(int q = 0; q < aggrPrior[p].size(); q++)
 				{
-					if(aggrPrior[p][q].find(variableNum) != -1)
+					if(aggrPrior[p][q] == i + 1)
 					{
 						existFlag = true; break;
 					}
@@ -519,57 +665,56 @@ void setPrior()
 			}
 			if(existFlag) continue;
 			int j = 0;
-			for(; j < VECO[i].size(); j++)
+			for(; j < VECO[i].size(); j++) // try to find if there is any other variable should be counted first while not counted yet
 			{
 				int first = 0, second = 0;
-				bool counted_prio_flag = true;
-				while(VECO[i][j].find("_", second + 1) != -1)
+				bool counted_prio_flag = true; // the flag for the priority, if there is any other higher 
+								//priority variable that is not counted, flag = false
+				while(VECO[i][j].find("_", second + 1) != -1) // find the high priority variables
 				{
 					counted_prio_flag = false;
 					first = VECO[i][j].find("_", second + 1);
 					second = VECO[i][j].find("_", first + 1);
-					string prevScanNum = VECO[i][j].substr(first, second - first + 1); 
-					for(int p = 0; p < aggrPrior.size(); p++)
+					int prevScanNum = convertToInt(VECO[i][j].substr(first + 1, second - first - 1)); // 
+					for(int p = 0; p < aggrPrior.size(); p++) // check if it is counted
 					{
 						for(int q = 0; q < aggrPrior[p].size(); q++)
 						{
-							if(aggrPrior[p][q].find(prevScanNum) != -1)
+							if(aggrPrior[p][q] == prevScanNum)
 							{
-								counted_prio_flag = true; break;
+								counted_prio_flag = true; break; // if it is counted, flag = true
 							}
 						}
-						if(counted_prio_flag) break;
+						if(counted_prio_flag) break; // if it is counted, flag = true
 					}
 					if(!counted_prio_flag) break;
 				}
-				if(!counted_prio_flag) break;
+				if(!counted_prio_flag) break; // if it is not counted, break the whole loop, j should < VECO[i].size()
 			}
-			if(j == VECO[i].size())
+			if(j == VECO[i].size()) // if there is no other variable should be counted first, set the priority
 			{
 				count++;
-				for(int p = 0; p < F.size(); p++)
-				{
-					if(F[p].find(variableNum) != -1) init.push_back(F[p]);
-				}
+				init.push_back(i + 1);
 			}
 		}
-		aggrPrior.push_back(init);
+		aggrPrior.push_back(init); // push back the set of variables that have the same priority
 		init.clear();
 	}
 }
 
 void setHashPara()
 {
+	if(VECO.size() == 0) return;
 	for(int i = 0; i < V.size(); i++)
 	{
 		string tempS = "="+V[i];
-		bool flag1 = true;
+		bool flag1 = true; // flag for the hash parameter
 		for(int p = 0; p < VECO.size(); p++)
 		{
-			bool flag2 = false;
+			bool flag2 = false; // flag for the existance of the grouping attribute in the range of the grouping variable
 			for(int q = 0; q < VECO[p].size(); q++)
 			{
-				if(VECO[p][q].find(tempS) != -1)
+				if(VECO[p][q].find(tempS) != -1 && (VECO[p][q].find(tempS) + tempS.length()) == VECO[p][q].length())
 				{
 					flag2 = true; break;
 				}
@@ -579,10 +724,166 @@ void setHashPara()
 				flag1 = false; break;
 			}
 		}
-		if(flag1) hashPara.push_back(V[i]);
+		if(flag1) hashPara.push_back(V[i]); // if true, it should be one of the hash parameter
 	}
 }
 
+string convertExp(string exp, string argu)
+{
+	if(exp == "&&" || exp == "||") return exp;
+	int first = exp.find_first_not_of('(');
+	int second = exp.find_first_of("=<>");
+	int third = exp.find_last_not_of(')');
+	string parenLeft, parenRight;
+	for(int i = 0; i < first; i++) parenLeft += "(";
+	for(int i = third + 1; i < exp.length(); i++) parenRight += ")";
 
+	string attribute = exp.substr(first, second - first);
+	if(attribute == "cust" || attribute == "prod" || attribute == "state")
+	{
+		attribute = "sale_rec." + attribute;
+		if(exp.find("='") != -1)
+		{
+			string value = exp.substr(exp.find("=") + 1, third - exp.find("="));
+			value[0]='\"'; value[third - exp.find("=") - 1] = '\"';
+			string convert;
+			return parenLeft + "strcmp(" + attribute + ", " + value + ") == 0" + parenRight;
+		}
+		else if(exp.find("<>'") != -1)
+		{
+			string value = exp.substr(exp.find("<>") + 2, third - exp.find("<>") - 1);
+			value[0]='\"'; value[third - exp.find("<>") - 2] = '\"';
+			return parenLeft + "strcmp(" + attribute + ", " + value + ") != 0" + parenRight;
+		}
+		else if(exp.find("=") != -1)
+		{
+			string value = argu + "." + exp.substr(exp.find("=") + 1, third - exp.find("="));
+			return parenLeft + "strcmp(" + attribute + ", " + value + ") == 0" + parenRight;
+		}
+		else if(exp.find("<>") != -1)
+		{
+			string value = argu + "." + exp.substr(exp.find("<>") + 2, third - exp.find("<>") - 1);
+			return parenLeft + "strcmp(" + attribute + ", " + value + ") != 0" + parenRight;
+		}
+	}
+	else
+	{
+		attribute = "sale_rec." + attribute;
+		int valueStart = exp.find_first_of("=<>");
+		string value = convertAggregates(exp.substr(valueStart), argu);
+		return parenLeft + attribute + exp.substr(second, valueStart - second) + value; 
+	}
+}
+string convertAggregates(string agg, string argu)
+{
+	int first = 0, second = 0;
+	if(agg.find_first_of("()+-*/", second) != 0)
+	{
+		if(agg.find_first_of("()+-*/", second) != -1)
+		{
+			second = agg.find_first_of("()+-*/", second);
+			string tempA = agg.substr(first, second - first - 1);
+			if(tempA.find("sum") != -1)
+			{
+				string tempB = tempA;
+				tempB.replace(tempB.find("sum"), 3, argu + ".sum");
+				agg.replace(agg.find(tempA), tempA.length(), tempB);
+				second += tempB.length() - tempA.length();
+			}
+			else if(tempA.find("count") != -1)
+			{
+				string tempB = tempA;
+				tempB.replace(tempB.find("count"), 5, argu + ".count");
+				agg.replace(agg.find(tempA), tempA.length(), tempB);
+				second += tempB.length() - tempA.length();
+			}
+			else if(tempA.find("avg") != -1)
+			{
+				string tempB = tempA, tempC = tempA;
+				tempB.replace(tempB.find("avg"), 3, argu + ".sum");
+				tempC.replace(tempC.find("avg"), 3, argu + ".count");
+				tempC = "(" + tempB + "/" + tempC + ")";
+				agg.replace(agg.find(tempA), tempA.length(), tempC);
+				second += tempC.length() - tempA.length();
+			}
+		}
+		else
+		{
+			second = agg.length();
+			string tempA = agg.substr(first, second - first);
+			if(tempA.find("sum") != -1)
+			{
+				string tempB = tempA;
+				tempB.replace(tempB.find("sum"), 3, argu + ".sum");
+				agg.replace(agg.find(tempA), tempA.length(), tempB);
+				second += tempB.length() - tempA.length();
+			}
+			else if(tempA.find("count") != -1)
+			{
+				string tempB = tempA;
+				tempB.replace(tempB.find("count"), 5, argu + ".count");
+				agg.replace(agg.find(tempA), tempA.length(), tempB);
+				second += tempB.length() - tempA.length();
+			}
+			else if(tempA.find("avg") != -1)
+			{
+				string tempB = tempA, tempC = tempA;
+				tempB.replace(tempB.find("avg"), 3, argu + ".sum");
+				tempC.replace(tempC.find("avg"), 3, argu + ".count");
+				tempC = "(" + tempB + "/" + tempC + ")";
+				agg.replace(agg.find(tempA), tempA.length(), tempC);
+				second += tempC.length() - tempA.length();
+			}
+		}
+	}
+	while(agg.find_first_of("()+-*/", second) != -1)
+	{
+		first = agg.find_first_of("()+-*/", second);
+		second = agg.find_first_of("()+-*/", first + 1);
+		if(second == -1)
+		{
+			
+		}
+		if(second - first == 1) continue;
+		string tempA = agg.substr(first + 1, second - first - 1);
+		if(tempA.find("sum") != -1)
+		{
+			string tempB = tempA;
+			tempB.replace(tempB.find("sum"), 3, argu + ".sum");
+			agg.replace(agg.find(tempA), tempA.length(), tempB);
+			second += tempB.length() - tempA.length();
+		}
+		else if(tempA.find("count") != -1)
+		{
+			string tempB = tempA;
+			tempB.replace(tempB.find("count"), 5, argu + ".count");
+			agg.replace(agg.find(tempA), tempA.length(), tempB);
+			second += tempB.length() - tempA.length();
+		}
+		else if(tempA.find("avg") != -1)
+		{
+			string tempB = tempA, tempC = tempA;
+			tempB.replace(tempB.find("avg"), 3, argu + ".sum");
+			tempC.replace(tempC.find("avg"), 3, argu + ".count");
+			tempC = "(" + tempB + "/" + tempC + ")";
+			agg.replace(agg.find(tempA), tempA.length(), tempC);
+			second += tempC.length() - tempA.length();
+		}
+	}
+	first = 0;
+	while(agg.find("=", first) != -1)
+	{
+		first = agg.find("=", first);
+		agg.replace(first, 1, "==");
+		first += 2;
+	}
+	first = 0;
+	while(agg.find("<>", first) != -1)
+	{
+		first = agg.find("<>", first);
+		agg.replace(first, 2, "!=");
+	}
+	return agg;
+}
 
 
